3e883a9f10dfa29adc738371c385fa3aa8e47ef1
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-11-06 12:26:43 +0100

    ADD: Generazione dinamica delle card

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 axios.get('https://jsonplaceholder.typicode.com/photos')
   .then(response => {
     const data = response.data;
+    const blogContainer = document.getElementById("blog-cards");
 
     for (let i = 0; i < 6; i++) {
       const photo = data[i];
+
+      const card = document.createElement("div");
+    card.className = "col-md-4 mb-4";
+
+    card.innerHTML = `
+          <div class="card">
+              <img src="${photo.url}" class="card-img-top" alt="">
+              <div class="card-body bg-light">
+                  <p class="card-text text-dark">${photo.title} <a href=""></a></p>
+              </div>
+          </div>
+      `;
+
+    blogContainer.appendChild(card);
       console.log('Title:', photo.title);
       console.log('URL:', photo.url);
     }
