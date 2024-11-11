7fc250a6f35684bbd29dac689805958a6d3f4411
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-11-11 07:24:17 +0100

    ADD: aggiunto bottone per il modale

--- js/script.js
+++ js/script.js
@@ -1,69 +1,57 @@
 axios
-  .get("https://jsonplaceholder.typicode.com/photos")
-  .then((response) => {
-    const data = response.data;
-    const blogContainer = document.getElementById("blog-cards");
-    const modalContainer = document.getElementById("modal-container");
+.get("https://jsonplaceholder.typicode.com/photos")
+.then((response) => {
+  const data = response.data;
+  const blogContainer = document.getElementById("blog-cards");
+  const modalContainer = document.getElementById("modal-container");
 
-    for (let i = 0; i < 6; i++) {
-      const photo = data[i];
+  for (let i = 0; i < 6; i++) {
+    const photo = data[i];
 
-      const card = document.createElement("div");
-      card.className = "column";
+    const card = document.createElement("div");
+    card.className = "column";
 
-      card.innerHTML = `
-          <div class="card"${photo.id}>
-          <img src="./img/pin.svg" class="pin-card" alt="">
-              <img src="${photo.url}" class="card-img-top img-size" alt="">
-              <div>
-                  <p class="card-text">${photo.title}</p>
-              </div>
-          </div>
-      `;
+    card.innerHTML = ` 
+  <div class="card" data-id="${photo.id}">
+      <img src="./img/pin.svg" class="pin-card" alt="">
+      <img src="${photo.url}" class="card-img-top img-size" alt="">
+      <div>
+          <p class="card-text">${photo.title}</p>
+      </div>
+  </div>
+`;
 
-      blogContainer.appendChild(card);
+    blogContainer.appendChild(card);
 
-      const modal = document.createElement("div");
-      modal.className = "modal";
-      modal.id = `modal-${photo.id}`;
+    const modal = document.createElement("div");
+    modal.className = "modal";
+    modal.id = `modal-${photo.id}`;
 
-      modal.innerHTML = `
-        <div class="modal-content">
-            <span onclick="closeModal(${photo.id})"></span>
-            <img src="${photo.url}" id="modal-image" alt="">
-            <p id="modal-title">${photo.title}</p>
-        </div>
-      `;
+    modal.innerHTML = `
+<div class="modal-content">
+    <img src="${photo.url}" id="modal-image" alt="">
+    <p id="modal-title">${photo.title}</p>
+    <button class="close-btn" onclick="closeModal(${photo.id})">Chiudi</button>
+</div>
+`;
 
-      modalContainer.appendChild(modal);
+    modalContainer.appendChild(modal);
 
-      card.addEventListener("click", function () {
-        openModal(photo.id);
-      });
-    }
-  })
-  .catch((error) => {
-    console.error("Errore nella richiesta:", error);
-  });
+    card.addEventListener("click", function () {
+      openModal(photo.id);
+    });
+  }
+})
+.catch((error) => {
+  console.error("Errore nella richiesta:", error);
+});
 
 function openModal(photoId) {
-  const modal = document.getElementById(`modal-${photoId}`);
-  modal.style.display = "flex";
+const modal = document.getElementById(`modal-${photoId}`);
+modal.style.display = "flex";
 }
 
 function closeModal(photoId) {
-  const modal = document.getElementById(`modal-${photoId}`);
-  modal.style.display = "none";
+const modal = document.getElementById(`modal-${photoId}`);
+modal.style.display = "none";
 }
-
-window.onclick = function (event) {
-  const modals = document.querySelectorAll(".modal");
-  modals.forEach((modal) => {
-    if (event.target === modal) {
-      modal.style.display = "none";
-    }
-  });
-};
-
-
-
